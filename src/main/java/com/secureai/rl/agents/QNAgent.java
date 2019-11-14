87d91fb118a3e203836f721b3bc44696f4a2af37
Andrea Silvi <andrea.silvi94@gmail.com>
2019-11-14 23:19:58 +0100

    Added RL4J

--- src/main/java/com/secureai/rl/agents/QNAgent.java
+++ src/main/java/com/secureai/rl/agents/QNAgent.java
@@ -1,34 +1,49 @@
 package com.secureai.rl.agents;
 
+import com.secureai.rl.abs.Action;
+import com.secureai.rl.abs.ActionSpace;
+import com.secureai.rl.abs.State;
+import com.secureai.rl.abs.StateSpace;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
+
+import java.util.Random;
 
 public class QNAgent {
 
     private INDArray qTable;
+    private StateSpace stateSpace;
+    private ActionSpace actionSpace;
+    private double learningRate;
+    private double discountFactor;
 
-    public QNAgent(long stateSize, long actionSize) {
-        this.qTable = Nd4j.zeros(stateSize, actionSize);
+    private Random random = new Random(12345);
+
+    public QNAgent(StateSpace stateSpace, ActionSpace actionSpace, double learningRate, double discountFactor) {
+        this.qTable = Nd4j.zeros(stateSpace.getSize(), actionSpace.getSize());
+        this.stateSpace = stateSpace;
+        this.actionSpace = actionSpace;
+        this.learningRate = learningRate;
+        this.discountFactor = discountFactor;
     }
 
-    public learn() {
-        //Q[state, action] = Q[state, action] + lr * (reward + gamma * np.max(Q[new_state, :]) — Q[state, action])
+    public void train(State state, Action action, State newState, double reward) {
+        int stateIndex = this.stateSpace.indexOf(state);
+        int actionIndex = this.actionSpace.indexOf(action);
+        int newStateIndex = this.stateSpace.indexOf(newState);
+
+        this.qTable.put(stateIndex, actionIndex, this.qTable.getDouble(stateIndex, actionIndex) + this.learningRate * (reward + this.discountFactor * this.qTable.getColumn(newStateIndex).maxNumber().doubleValue() - this.qTable.getDouble(stateIndex, actionIndex)));
     }
 
-    public int choose() {
+    public Action choose(State state) {
+        double epsilon = 0.2;
+        if(this.random.nextDouble() <= epsilon) {
+            int randomActionIndex = random.nextInt(actionSpace.getSize());
+            return this.actionSpace.get(randomActionIndex);
+        }
 
-        /*
-        # Set the percent you want to explore
-epsilon = 0.2
-if random.uniform(0, 1) < epsilon:
-    """
-    Explore: select a random action
-    """
-else:
-    """
-    Exploit: select the action with max value (future reward)
-
-         */
-
+        int stateIndex = this.stateSpace.indexOf(state);
+        int maxActionIndex = this.qTable.getColumn(stateIndex).argMax().getInt(0);
+        return this.actionSpace.get(maxActionIndex);
     }
 }
