38f5c84531f80dd81a731b8ae4a34d2c3c466a04
Andrea Silvi <andrea.silvi94@gmail.com>
2019-11-28 12:55:32 +0100

    Working on definition and filter

---
+++ src/main/java/com/secureai/nn/FilteredMultiLayerNetwork.java
@@ -0,0 +1,35 @@
+package com.secureai.nn;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.nd4j.linalg.api.ndarray.INDArray;
+
+public class FilteredMultiLayerNetwork extends MultiLayerNetwork {
+
+    @Getter
+    @Setter
+    private MultiLayerNetworkPredictionFilter multiLayerNetworkPredictionFilter;
+
+    public FilteredMultiLayerNetwork(MultiLayerConfiguration conf) {
+        super(conf);
+    }
+
+    public FilteredMultiLayerNetwork(String conf, INDArray params) {
+        super(conf, params);
+    }
+
+    public FilteredMultiLayerNetwork(MultiLayerConfiguration conf, INDArray params) {
+        super(conf, params);
+    }
+
+    @Override
+    public int[] predict(INDArray d) {
+        return this.multiLayerNetworkPredictionFilter.run(d, super.predict(d));
+    }
+
+    public interface MultiLayerNetworkPredictionFilter {
+        int[] run(INDArray input, int[] output);
+    }
+}
