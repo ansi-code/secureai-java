29bea5460c936602ed9729bd33b94d02b7dbe2c6
Andrea Silvi <andrea.silvi94@gmail.com>
2020-01-21 17:30:18 +0100

    WIP on input and output maps

--- src/main/java/com/secureai/dyn/DynMultiLayerNetwork.java
+++ src/main/java/com/secureai/dyn/DynMultiLayerNetwork.java
@@ -1,9 +1,19 @@
 package com.secureai.dyn;
 
+import com.secureai.utils.BidirectionalMap;
+import lombok.Getter;
+import lombok.Setter;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.nd4j.linalg.api.ndarray.INDArray;
 
 public class DynMultiLayerNetwork extends FilteredMultiLayerNetwork {
+
+    @Getter @Setter
+    private BidirectionalMap<?, Integer> inputMap;
+
+    @Getter @Setter
+    private BidirectionalMap<?, Integer> outputMap;
+
     public DynMultiLayerNetwork(MultiLayerConfiguration conf) {
         super(conf);
     }
@@ -15,4 +25,5 @@ public class DynMultiLayerNetwork extends FilteredMultiLayerNetwork {
     public DynMultiLayerNetwork(MultiLayerConfiguration conf, INDArray params) {
         super(conf, params);
     }
+
 }
