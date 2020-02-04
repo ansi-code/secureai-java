34717a517d02880e683c6ba7a67a93eab2427ce0
Andrea Silvi <andrea.silvi94@gmail.com>
2020-02-04 13:14:14 +0100

    Minor fix to sequential tests

--- test/main/java/com/secureai/SequentialTest.java
+++ test/main/java/com/secureai/SequentialTest.java
@@ -2,9 +2,9 @@ package com.secureai;
 
 public class SequentialTest {
     public static void main(String[] args) throws Exception {
-        DQNMain.main("layers", "1");
-        DQNMain.main("layers", "2");
-        DQNMain.main("layers", "3");
-        DQNMain.main("layers", "4");
+        DQNMain.main("--layers", "4");
+        DQNMain.main("--layers", "2");
+        DQNMain.main("--layers", "3");
+        DQNMain.main("--layers", "4");
     }
 }
