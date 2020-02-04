5e7c3b91163defc02fb2807090cae155ab51dd04
Andrea Silvi <andrea.silvi94@gmail.com>
2020-02-04 22:57:13 +0100

    Uncommented things in test

--- src/main/java/com/secureai/SeqMain.java
+++ src/main/java/com/secureai/SeqMain.java
@@ -2,7 +2,6 @@ package com.secureai;
 
 public class SeqMain {
     public static void main(String[] args) throws Exception {
-        /*
         // THIS IS THE TEST FOR CPU/GPU and layers comparison
         DQNMain.main("--layers", "1", "--topolgy", "0");
         DQNMain.main("--layers", "2", "--topolgy", "0");
@@ -18,7 +17,6 @@ public class SeqMain {
         DQNMain.main("--layers", "2", "--topolgy", "3");
         DQNMain.main("--layers", "3", "--topolgy", "3");
         DQNMain.main("--layers", "4", "--topolgy", "3");
-         */
 
         // THIS IS THE TEST FOR DQN GAMMA
         DQNMain.main("--layers", "3", "--topolgy", "paper-4", "--gamma", "5");
@@ -31,13 +29,12 @@ public class SeqMain {
 
         // THIS IS THE TEST FOR DynDDQN
         DynDQNMain.main("--layers", "3", "--gamma", "5");
+        DynDQNMain.main("--layers", "3", "--gamma", "5", "--doubleDQN", "true");
 
-        /*
         // THIS IS THE TEST FOR STANDARD/PARALLEL/SPARK
         DQNMain.main("--layers", "3", "--topolgy", "paper-4", "--dqn", "standard");
         DQNMain.main("--layers", "3", "--topolgy", "paper-4", "--dqn", "parallel");
         DQNMain.main("--layers", "3", "--topolgy", "paper-4", "--dqn", "spark");
-         */
 
         // THIS IS THE TEST FOR VI/QN/DQN
         QNMain.main("--topolgy", "paper-4");
